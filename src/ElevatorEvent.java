5464cab656b00da4d6d0b5b09942d7eae48fbda6
Mariam05 <tomariam@gmail.com>
2020-01-28 20:07:24 -0500

    Iteration 1 Framework

---
+++ src/ElevatorEvent.java
@@ -0,0 +1,9 @@
+/**
+ * This class will be used to created event objects.
+ * Each event object will have the following attributes associated with it:
+ * Time, Floor or elevator number, button
+ * 
+ */
+public class ElevatorEvent {
+
+}
