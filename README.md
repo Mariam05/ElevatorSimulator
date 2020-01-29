a609bdc2c232eb359e4d4d5aadd4aa3bcdf63f1b
Mariam05 <tomariam@gmail.com>
2020-01-28 21:14:47 -0500

    Added data file

---
+++ README.md
@@ -0,0 +1,10 @@
+# Elevator Control System and Simulator
+
+#### Iteration 1 - Establish Connection Between the Three Subsystems
+
+##### Requirements:
+<ul> <li> “README.txt” file explaining the names of your files, set up instructions, etc. </li>
+<li> Breakdown of responsibilities of each team member for this iteration. </li>
+<li> UML class diagram and sequence diagrams. </li>
+<li> Detailed set up and test instructions, including test files used </li>
+<li> Code (.java files, all required Eclipse files, etc.) </li> </ul>
--- src/ElevatorEvent.java
+++ src/ElevatorEvent.java
@@ -1,3 +1,5 @@
+import java.sql.Time;
+
 /**
  * This class will be used to created event objects.
  * Each event object will have the following attributes associated with it:
@@ -6,4 +8,15 @@
  */
 public class ElevatorEvent {
 
+	private Time time;
+	private int floor;
+	private boolean floorButton; // true if up, false if down c
+	private int destinationFloor;
+	
+	public ElevatorEvent(String time, int floor, boolean floorButton, int destinationFloor) {
+		this.time = Time.valueOf(time);
+		this.destinationFloor = destinationFloor;
+		this.floor = floor;
+		this.floorButton = floorButton;
+	}
 }
